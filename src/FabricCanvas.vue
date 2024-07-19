7036914c79b265e78a7fa7f9977424c7e9071583
翟党伟 <442283045@qq.com>
2024-07-19 11:34:26 +0800

    another work

--- src/FabricCanvas.vue
+++ src/FabricCanvas.vue
@@ -3,7 +3,7 @@ import * as fabric from 'fabric'
 import imgUrl from '@/assets/wallhaven.jpg'
 import { onMounted } from 'vue'
 onMounted(init)
-type Mode = '选择' | '移动' | '画笔'
+type Mode = '选择' | '移动' | '画笔' | '文字'
 let canvas: fabric.Canvas
 let mode: Mode = '选择'
 let isMoving = false
@@ -12,7 +12,8 @@ let redoStack: string[] = []
 let pencilBrush: fabric.PencilBrush
 let currentEditImg: fabric.FabricImage
 let dirty = false // 是否有未保存的更改
-
+const CANVAS_WIDTH = 800
+const CANVAS_HEIGHT = 600
 const canvasZoomRatio = ref(100)
 
 function setMode(newMode: Mode) {
@@ -60,13 +61,45 @@ function initEventListener() {
       canvas.requestRenderAll()
     }
   })
+
   canvas.on('mouse:down', (opt) => {
-    if (opt && opt.e && mode === '移动') {
+    if (mode === '移动' && opt && opt.e) {
       isMoving = true
     }
 
     if (mode === '画笔' && dirty === true) {
       saveState()
+    }
+
+    if (mode === '文字') {
+      // 用户已经输入文字，再次点击其他区域，渲染文字，退出文字模式
+      if (activeTextBox) {
+        if (activeTextBox.text.trim()) {
+          completeTextInput()
+          return
+        } else {
+          // 去掉先前的输入框
+          activeTextBox.exitEditing()
+          canvas.remove(activeTextBox)
+        }
+      }
+
+      const pointer = canvas.getViewportPoint(opt.e)
+      const textbox = new fabric.Textbox('', {
+        left: pointer.x,
+        top: pointer.y,
+        width: 150,
+        fontSize: 20,
+        borderColor: 'yellow',
+        hasControls: true,
+        fill: 'red',
+      })
+
+      canvas.add(textbox)
+      canvas.setActiveObject(textbox)
+      textbox.enterEditing()
+      textbox.hiddenTextarea!.focus()
+      activeTextBox = textbox
     }
   })
   canvas.on('mouse:up', (opt) => {
@@ -91,21 +124,38 @@ function initEventListener() {
     canvasZoomRatio.value = Math.round(canvas.getZoom() * 100)
     opt.e.preventDefault()
     opt.e.stopPropagation()
+    console.log(canvas.toJSON())
   })
 }
-// 保存画布状态
+
+function completeTextInput() {
+  if (activeTextBox && activeTextBox.text.trim()) {
+    activeTextBox.exitEditing()
+    // canvas.remove(activeTextBox) // Optionally disable text mode after adding text
+    canvas.renderAll()
+    toggleMode('选择')
+  }
+}
+
+/**
+ * 保存画布状态
+ */
 function saveState(resetRedo = true) {
   history.push(canvas.toJSON())
   if (resetRedo) {
     redoStack = []
   }
+  dirty = false
   console.log('state saved', history)
 }
 
 // 撤销
 async function handleUndo() {
-  if (history.length === 1) {
+  if (history.length === 1 && dirty === true) {
+    saveState(false)
+    redoStack.push(history.pop()!)
     await canvas.loadFromJSON(history[0])
+    dirty = false
   }
   if (history.length > 1) {
     if (dirty === true) {
@@ -131,15 +181,17 @@ async function handleRedo() {
       saveState(false)
     }
     await canvas.loadFromJSON(redoState)
+    dirty = true
     canvas.renderAll()
   }
   console.log('redo', redoStack)
+  console.log('undo', history)
 }
 
 async function init() {
   canvas = new fabric.Canvas('canvas', {
-    width: 800,
-    height: 600,
+    width: CANVAS_WIDTH,
+    height: CANVAS_HEIGHT,
     backgroundColor: 'rgb(245, 245, 245)',
   })
   canvas.preserveObjectStacking = true // 在选择时保持对象的堆叠顺序
@@ -174,6 +226,7 @@ function handleRotate(direction: 'left' | 'right') {
 function handleZoomIn() {
   const centerX = canvas.getWidth() / 2
   const centerY = canvas.getHeight() / 2
+  console.log(centerX, centerY)
   canvas.zoomToPoint(new fabric.Point({ x: centerX, y: centerY }), canvas.getZoom() + 0.1)
   canvasZoomRatio.value = Math.round(canvas.getZoom() * 100)
 }
@@ -208,6 +261,9 @@ function toggleMode(mode: Mode) {
     case '画笔':
       handleBrush()
       break
+    case '文字':
+      handleText()
+      break
   }
   canvas.renderAll()
 }
@@ -232,6 +288,32 @@ function handleMove() {
   })
   canvas.defaultCursor = 'grab'
 }
+
+// 文字模式
+let activeTextBox: fabric.Textbox
+function handleText() {}
+function handleDownload() {
+  // 为了导出图片，需要将画布缩放，偏移等操作还原
+  const originalZoom = canvas.getZoom()
+  const originalVpt = canvas.viewportTransform.slice() as fabric.TMat2D
+  canvas.setZoom(1.0)
+  canvas.setViewportTransform([1, 0, 0, 1, 0, 0])
+  const dataURL = canvas.toDataURL({
+    format: 'png', // You can change this to 'jpeg' or other supported formats
+    quality: 1.0, // Quality for JPEGs (0 to 1 scale). Not applicable for PNGs
+    multiplier: 1,
+  })
+  canvas.setZoom(originalZoom)
+  canvas.setViewportTransform(originalVpt)
+
+  // Create a temporary link to trigger the download
+  const link = document.createElement('a')
+  link.href = dataURL
+  link.download = 'canvas-image.png' // Suggest a filename for the download
+  document.body.appendChild(link) // Append to the document
+  link.click() // Trigger the download
+  document.body.removeChild(link) // Clean up
+}
 const modes = ref<{ label: Mode; event: Function }[]>([
   {
     label: '选择',
@@ -244,6 +326,10 @@ const modes = ref<{ label: Mode; event: Function }[]>([
   {
     label: '画笔',
     event: handleBrush,
+  },
+  {
+    label: '文字',
+    event: handleText,
   },
 ])
 </script>
@@ -294,9 +380,10 @@ const modes = ref<{ label: Mode; event: Function }[]>([
           :title="mode.label"
         >
           <template #icon>
-            <div v-if="mode.label === '选择'" class="i-lucide:mouse-pointer-2 w-1em h-1em"></div>
+            <div v-if="mode.label === '选择'" class="i-lucide:mouse-pointer-2"></div>
             <div v-if="mode.label === '移动'" class="i-lucide:hand"></div>
-            <div v-if="mode.label === '画笔'" class="i-lucide:pencil w-1em h-1em"></div>
+            <div v-if="mode.label === '画笔'" class="i-lucide:pencil"></div>
+            <div v-if="mode.label === '文字'" class="i-icon-park-outline:add-text"></div>
           </template>
         </n-button>
       </n-space>
@@ -345,6 +432,19 @@ const modes = ref<{ label: Mode; event: Function }[]>([
         >
           <template #icon>
             <div class="i-lucide:rotate-cw"></div>
+          </template>
+        </n-button>
+      </n-space>
+      <n-space align="center" class="bg-[#ECECF4] rounded-md">
+        <n-button
+          :bordered="false"
+          class="hover:bg-[#f1f0ff]!"
+          @click="handleDownload"
+          title="导出"
+          :focusable="false"
+        >
+          <template #icon>
+            <div class="i-carbon:document-download"></div>
           </template>
         </n-button>
       </n-space>
